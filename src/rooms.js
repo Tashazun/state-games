bf0a728c8692cd7867cf613ec2723622e18be07c
Tasha R Zuniga <Tashazun@gmail.com>
2018-06-06 11:12:36 -0700

    room description start

--- src/rooms.js
+++ src/rooms.js
@@ -1,10 +1,13 @@
 
 
 const centerRoom = {
-    title: asdf,
+    title: 'The Altar',
     image: asdfds,
-    description: S_IFDIR,
+    description: `The floor is cold where you lay in a dazed, half-wakefullness. It sends a shiver through your limbs, but also works to soothe your throbbing headache. After several drawn out minutes you work up to a sitting position to study your surroundings through the thin light. Candles are burning low, scattered throughout the room and casting long shadows over dusty books, rows of opaque bottles and jars on the shelves, and the large pentagram painted in what looks like blood on the floor before you. Terror takes hold and forces you to move, scrambling up you spin around to look for a way out, and find there are four plain, wooden doors in each direction.`, 
     items: [],
+    prevent: '',
+    question: '',
+    answer: '',
     doors: {
         n: northDoor,
         e: eastDoor,
@@ -14,10 +17,13 @@ const centerRoom = {
 },
 
 const northDoor = {
-    title: asdfsd,
+    title: 'The Mirror',
     image: sdfsdf,
-    description: afsdf,
+    description: 'You open the door on the northern most door and behind it you find an empty room save for a tall mirror hung on the the wall to your right. Entering cautiously you move to stand before it and see that in it\'s gleaming surface is a reflection of of the far wall. Written upon it is the letters "NAHTAIVEL" and beneath it is a wooden box on a table. You turn around in confusion and find that there is only the table in the room with you.',
     items: [],
+    prevent: '',
+    question: '',
+    answer: 'LEVIATHAN',
     doors: {
         s: southDoor
     }
@@ -25,7 +31,7 @@ const northDoor = {
 const eastDoor = {
     title: asdfsd,
     image: sdfsdf,
-    description: afsdf,
+    description: 'You enter through the east most door and in the room beyond find a low table with five coins',
     items: [],
     doors: {
         w: westDoor
