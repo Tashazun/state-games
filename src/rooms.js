763b88c58f145ff93427d813aff1946e9b9ec0f9
Ryan Manro <rmanro@gmail.com>
2018-06-06 11:23:24 -0700

    edited rooms.js

--- src/rooms.js
+++ src/rooms.js
@@ -1,7 +1,7 @@
 const centerRoom = {
-  title: 'The Center Room',
+  title: 'The Altar',
   image: 'https://images.gamewatcherstatic.com/image/file/8/9f/81008/large_20161023000504_1.jpg',
-  description: 'At Center Room',
+  description: 'The floor is cold where you lay in a dazed, half-wakefullness. It sends a shiver through your limbs, but also works to soothe your throbbing headache. After several drawn out minutes you work up to a sitting position to study your surroundings through the thin light. Candles are burning low, scattered throughout the room and casting long shadows over dusty books, rows of opaque bottles and jars on the shelves, and the large pentagram painted in what looks like blood on the floor before you. Terror takes hold and forces you to move, scrambling up you spin around to look for a way out, and find there are four plain, wooden doors in each direction.',
   items: [],
   doors: {
     n: 'northRoom',
@@ -12,10 +12,13 @@ const centerRoom = {
 };
 
 const northRoom = {
-  title: 'The North Room',
+  title: 'The Mirror',
   image: 'https://toghs.files.wordpress.com/2008/02/inside-of-cabin1.jpg',
-  description: 'At North Room',
+  description: 'You open the door on the northern most door and behind it you find an empty room save for a tall mirror hung on the the wall to your right. Entering cautiously you move to stand before it and see that in it\'s gleaming surface is a reflection of of the far wall. Written upon it is the letters "NAHTAIVEL" and beneath it is a wooden box on a table. You turn around in confusion and find that there is only the table in the room with you.',
   items: [],
+  prevent: '',
+  question: '',
+  answer: 'LEVIATHAN',
   doors: {
     s: 'centerRoom'
   }
@@ -25,7 +28,7 @@ const northRoom = {
 const eastRoom = {
   title: 'The East Room',
   image: 'https://i.imgur.com/tfdjJ5Gr.jpg',
-  description: 'At East Room',
+  description: 'You enter through the east most door and in the room beyond find a low table with five coins',
   items: [],
   doors: {
     w: 'centerRoom'
@@ -60,60 +63,9 @@ export const rooms = {
   westRoom
 };
 
+
 export const start = centerRoom;
 
-const centerRoom = {
-    title: 'The Altar',
-    image: asdfds,
-    description: `The floor is cold where you lay in a dazed, half-wakefullness. It sends a shiver through your limbs, but also works to soothe your throbbing headache. After several drawn out minutes you work up to a sitting position to study your surroundings through the thin light. Candles are burning low, scattered throughout the room and casting long shadows over dusty books, rows of opaque bottles and jars on the shelves, and the large pentagram painted in what looks like blood on the floor before you. Terror takes hold and forces you to move, scrambling up you spin around to look for a way out, and find there are four plain, wooden doors in each direction.`, 
-    items: [],
-    prevent: '',
-    question: '',
-    answer: '',
-    doors: {
-        n: northDoor,
-        e: eastDoor,
-        w: westDoor,
-        s: southDoor
-    }
-},
 
-const northDoor = {
-    title: 'The Mirror',
-    image: sdfsdf,
-    description: 'You open the door on the northern most door and behind it you find an empty room save for a tall mirror hung on the the wall to your right. Entering cautiously you move to stand before it and see that in it\'s gleaming surface is a reflection of of the far wall. Written upon it is the letters "NAHTAIVEL" and beneath it is a wooden box on a table. You turn around in confusion and find that there is only the table in the room with you.',
-    items: [],
-    prevent: '',
-    question: '',
-    answer: 'LEVIATHAN',
-    doors: {
-        s: southDoor
-    }
-}
-const eastDoor = {
-    title: asdfsd,
-    image: sdfsdf,
-    description: 'You enter through the east most door and in the room beyond find a low table with five coins',
-    items: [],
-    doors: {
-        w: westDoor
-    }
-}
-const southDoor = {
-    title: asdfsd,
-    image: sdfsdf,
-    description: afsdf,
-    items: [],
-    doors: {
-        n: northDoor
-    }
-}
-const westDoor = {
-    title: asdfsd,
-    image: sdfsdf,
-    description: afsdf,
-    items: [],
-    doors: {
-        e: eastDoor
-    }
-}
+
+
